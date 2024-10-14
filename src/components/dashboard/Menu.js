a32db957aa06e13e8fa449853e38d344313996cd
DMancia03 <diego.mancia.03@gmail.com>
2024-10-13 19:08:15 -0600

    Agregando pantallas existentes al menu

--- src/components/dashboard/Menu.js
+++ src/components/dashboard/Menu.js
@@ -3,9 +3,36 @@ import { Text, View, ScrollView, StyleSheet, Alert, TouchableOpacity } from "rea
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Menu = ({ navigation }) => {
+    //Navegar entre pantallas
+    const verNotas = () => {
+        navigation.navigate('Notas');
+    };
+
+    const verRecordatorios = () => {
+        navigation.navigate('Recordatorio');
+    }
+
+    const VerEtiquetas = () => {
+        navigation.navigate('Etiquetas');
+    }
+
     return (
         <View>
-            <Text>Hola</Text>
+            <TouchableOpacity onPress={() => verNotas()}>
+                <Text>Notas</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => verRecordatorios()}>
+                <Text>Recordatorios</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => VerEtiquetas()}>
+                <Text>Etiquetas</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>Papelera de reciclaje</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>Cerrar sesión</Text>
+            </TouchableOpacity>
         </View>
     )
 }
