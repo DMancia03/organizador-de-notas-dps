4ab483f757488639b75e4b9d1cb5e52f7db93cc4
DMancia03 <diego.mancia.03@gmail.com>
2024-10-13 18:20:13 -0600

    Agregando ver notas

---
+++ src/screens/VerNotas.js
@@ -0,0 +1,44 @@
+import react, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Notas from "../components/notas/Notas";
+import GuardarNota from "../components/notas/GuardarNota";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const VerNotas = () => {
+    return (
+        <>
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="Notas" 
+                    initialParams={{ "ultimaAccion" : 'inicio' }}
+                    component={Notas}
+                    options={{
+                        title: 'Notas'
+                    }}
+                />
+                <Stack.Screen 
+                    name="CrearNota"
+                    initialParams={{ "accion" : 'crear' }} 
+                    component={GuardarNota}
+                    options={{
+                        title: 'Crear nota'
+                    }}
+                />
+                <Stack.Screen 
+                    name="EditarNota"
+                    initialParams={{ "accion" : 'editar', 'id': 0 }} 
+                    component={GuardarNota}
+                    options={{
+                        title: 'Editar nota'
+                    }}
+                />
+            </Stack.Navigator>
+        </>
+    );
+}
+
+export default VerNotas;
+
